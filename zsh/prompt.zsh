8cfdcc04df2e6dab7869f47808a61588693bc06a
Yarden Sod-Moriah <yardnsm@gmail.com>
2016-02-24 14:15:47 +0200

    improved git remote status and tweaked zsh

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -8,11 +8,17 @@ autoload -U promptinit && promptinit
 local PROMPT_SYMBOL="❯"
 local CLEAN_SYMBOL="☀"
 local DIRTY_SYMBOL="☂"
+local NEEDS_PULL_SYMBOL="✦"
+local NEEDS_PUSH_SYMBOL="☁"
+local DIVERGED_SYMBOL="[DIVERGED]"
 
 # For the git status
 ZSH_THEME_GIT_PROMPT_SUFFIX="$reset_color"
 ZSH_THEME_GIT_PROMPT_CLEAN="$fg[green]$CLEAN_SYMBOL "
 ZSH_THEME_GIT_PROMPT_DIRTY="$fg[red]$DIRTY_SYMBOL "
+ZSH_THEME_GIT_NEEDS_PULL="$fg[lightred]$NEEDS_PULL_SYMBOL "
+ZSH_THEME_GIT_NEEDS_PUSH="$fg_bold[magenta]$NEEDS_PUSH_SYMBOL "
+ZSH_THEME_GIT_DIVERGED="$fg_bold[red]$DIVERGED_SYMBOL "
 
 # The prompt
 PROMPT='
@@ -26,7 +32,7 @@ RPROMPT="%T"
 local current_dir="%{$fg[white]%}%3~%{$reset_color%} "
 local arrow="%(?.%{$fg[cyan]%}.%{$fg[red]%})%B${PROMPT_SYMBOL}%b"
 
-# user@host for SSH connections
+# Show user@host for SSH connections
 function user_host() {
 
     # Make the color red if is root
@@ -46,18 +52,18 @@ function user_host() {
 	fi
 }
 
-# Put the symbol before branch
+# Git info (clean/dirty, needs pull/push)
 function git_prompt_info() {
 	ref=$(command git symbolic-ref HEAD 2> /dev/null) || \
 	ref=$(command git rev-parse --short HEAD 2> /dev/null) || return 0
-	echo "$(parse_git_dirty)$(git_needs_push)${ref#refs/heads/}$ZSH_THEME_GIT_PROMPT_SUFFIX"
+	echo "$(parse_git_dirty)$(git_remote_status)${ref#refs/heads/}$ZSH_THEME_GIT_PROMPT_SUFFIX"
 }
 
 # Checks if working tree is dirty
 parse_git_dirty() {
 	local STATUS=''
 	local FLAGS
-	FLAGS=('--porcelain')
+    FLAGS=('--porcelain')
 	if [[ "$(command git config --get oh-my-zsh.hide-dirty)" != "1" ]]; then
 		if [[ $POST_1_7_2_GIT -gt 0 ]]; then
 			FLAGS+='--ignore-submodules=dirty'
@@ -74,12 +80,20 @@ parse_git_dirty() {
 	fi
 }
 
-# Check if git needs push
-git_needs_push() {
-  if [[ $(git cherry -v @{upstream} 2>/dev/null) == "" ]]
-  then
-    echo ""
-  else
-    echo "%{$fg_bold[magenta]%}☁%f "
-  fi
+# Check git remote status
+function git_remote_status() {
+
+    local git_local=$(git rev-parse @)
+    local git_remote=$(git rev-parse @{u})
+    local git_base=$(git merge-base @ @{u})
+
+    if [ git_local = git_remote ]; then
+        echo ""
+    elif [ git_local = git_base ]; then
+        echo "$ZSH_THEME_GIT_NEEDS_PULL"
+    elif [ git_remote = git_base ]; then
+        echo "$ZSH_THEME_GIT_NEEDS_PUSH"
+    else
+        echo "$ZSH_THEME_GIT_DIVERGED"
+    fi
 }
