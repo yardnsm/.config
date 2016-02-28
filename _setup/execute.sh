295ddf2787915274c74d0cff72a672d225a82242
Yarden Sod-Moriah <yardnsm@gmail.com>
2016-02-28 18:03:25 +0200

    update tasks names

--- _setup/execute.sh
+++ _setup/execute.sh
@@ -19,51 +19,51 @@ print_info "Setup Local gitconfig"
 source ./_setup/tasks/gitconfig.sh
 
 # --------------------------------------------- #
-# | Creating symlinks and copies
+# | Create symlinks and copies
 # --------------------------------------------- #
-print_info "Creating symlinks and copies"
+print_info "Create symlinks and copies"
 source ./_setup/tasks/symlink_and_copy.sh
 
 # --------------------------------------------- #
-# | Setting up OSX
+# | Setup OSX configurations
 # --------------------------------------------- #
 print_info "Setup OSX configurations"
 source ./osx/run_configurations.sh
 
 # --------------------------------------------- #
-# | Installing Homebrew and Cask
+# | Install Homebrew and Cask
 # --------------------------------------------- #
-print_info "Installing Homebrew and Cask"
+print_info "Install Homebrew and Cask"
 source ./osx/homebrew/install_homebrew_cask.sh
 
 # --------------------------------------------- #
-# | Installing Homebrew dependencies
+# | Install Homebrew dependencies
 # --------------------------------------------- #
-print_info "Installing Homebrew dependencies"
+print_info "Install Homebrew dependencies"
 source ./osx/homebrew/install_dependencies.sh
 
 # --------------------------------------------- #
-# | Installing Applications
+# | Install Applications
 # --------------------------------------------- #
-print_info "Installing Applications"
+print_info "Install Applications"
 source ./osx/applications/install_applications.sh
 
 # --------------------------------------------- #
-# | Changing applications settings
+# | Change applications settings
 # --------------------------------------------- #
-print_info "Setting applications settings"
+print_info "Change applications settings"
 source ./osx/applications/set_applications_settings.sh
 
 # --------------------------------------------- #
 # | Run the 'misc' script
 # --------------------------------------------- #
-print_info "Running miscellaneous"
+print_info "Run miscellaneous"
 source ./_setup/tasks/misc.sh
 
 # --------------------------------------------- #
 # | Finito
 # --------------------------------------------- #
-print_info "Cleaning up..."
+print_info "Clean up..."
 source ./_setup/tasks/cleanup.sh
 
 # Say that we done
