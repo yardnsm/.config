42f76f80c918855fb88a38cdceed8a2cfcbb6f60
Yarden Sod-Moriah <yardnsm@gmail.com>
2016-05-22 16:00:23 +0300

    oops

--- homebrew/main.sh
+++ homebrew/main.sh
@@ -5,6 +5,9 @@ current_dir="$(dirname "$BASH_SOURCE")"
 # --------------------------------------------- #
 # | Homebrew
 # --------------------------------------------- #
+print_info "Updating Homebrew"
+source $current_dir/scripts/update_homebrew.sh
+
 print_info "Install Homebrew and its taps"
 source $current_dir/scripts/install_homebrew.sh
 
--- homebrew/scripts/update_homebrew.sh
+++ homebrew/scripts/update_homebrew.sh
@@ -6,4 +6,6 @@
 if cmd_exists 'brew'; then
     execute "brew update" "brew update"
     execute "brew cask update" "brew cask update"
+else
+    print_status "Homebrew is not installed, no need to update"
 fi
