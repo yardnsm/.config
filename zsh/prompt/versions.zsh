249021baccd7293698035070b531199d11bfd12a
Yarden Sod-Moriah <yardnsm@gmail.com>
2016-05-27 16:51:39 +0300

    fixed prompt

--- zsh/prompt/versions.zsh
+++ zsh/prompt/versions.zsh
@@ -4,12 +4,12 @@
 
 # All versions
 function versions_info() {
-    echo "$(version_node) "
+    echo "$(version_node)"
 }
 
 # Get nodejs version if there is a 'package.json' file
 function version_node () {
     [[ ! -f "$(pwd)/package.json" ]] && return
     local node_version=$(node -v 2>/dev/null)
-    [[ ! -z "${node_version}" ]] && echo "${THEME_VERSION_NODEJS_PREFIX}${node_version:1}${THEME_VERSION_NODEJS_SUFFIX}"
+    [[ ! -z "${node_version}" ]] && echo "${THEME_VERSION_NODEJS_PREFIX}${node_version:1}${THEME_VERSION_NODEJS_SUFFIX} "
 }
