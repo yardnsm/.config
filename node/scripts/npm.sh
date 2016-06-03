0c2bd294793840f9d8d56fd38572b7432d823186
Yarden Sod-Moriah <yardnsm@gmail.com>
2016-06-03 12:17:07 +0300

    .

--- node/scripts/npm.sh
+++ node/scripts/npm.sh
@@ -3,11 +3,15 @@
 # --------------------------------------------- #
 # | Install NPM Global Packages
 # --------------------------------------------- #
+
+# Dev stuff
 npm_install 'bower'
 npm_install 'http-server'
 npm_install 'gulp'
 npm_install 'jade'
 npm_install 'nodemon'
-npm_install 'speed-test'
 npm_install 'json-server'
 npm_install 'n'
+
+# Misc
+npm_install 'speed-test'
