64de9e4a41e8b584ceadcf3f6c848b45b4ec6de7
Yarden Sod-Moriah <yardnsm@gmail.com>
2017-07-23 14:54:23 +0300

    Update yolo

--- npm/functions.zsh
+++ npm/functions.zsh
@@ -2,11 +2,11 @@ function yolo() {
   local install_cmd='npm install'
 
   if [[ -f yarn.lock ]]; then
-    install_cmd='yarn'
+    install_cmd='yarn install --no-lockfile'
   fi
 
   echo "\n  > rm -rf node_modules && $install_cmd\n"
   sleep 2
 
-  rm -rf node_modules && $install_cmd
+  eval "rm -rf node_modules && $install_cmd"
 }
