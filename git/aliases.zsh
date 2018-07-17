2a739e0d40fac291959e51e09f2caf04bc2c3018
Yarden Sod-Moriah <yardnsm@gmail.com>
2018-07-17 19:41:44 +0300

    git: updated git aliases and git zsh aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,16 +1,16 @@
 alias g='git'
 alias gs='git status -sb'
 alias ga='git add'
+alias gaa='git add .'
 alias gc='git commit'
+alias gac='git add . && git commit'
+alias gcl='git clone'
+alias gco='git checkout'
 alias gb='git branch'
 alias gl='git pull'
 alias gp='git push'
 alias gpyolo='git push --no-verify'
 alias gd='git diff'
-alias gac='git add . && git commit'
-
-# How many commits?
-alias git-count='git shortlog -sn'
 
 # Go back to the root of the git repo
 alias gr='git rev-parse 2>/dev/null && cd "./$(git rev-parse --show-cdup)"'
