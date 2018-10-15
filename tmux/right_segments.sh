73b5cd37de0b793401ef88d7f1b42146826bee76
Yarden Sod-Moriah <yardnsm@gmail.com>
2018-10-15 18:28:04 +0300

    tmux: added headphone segment, cleanup

--- tmux/right_segments.sh
+++ tmux/right_segments.sh
@@ -3,19 +3,14 @@
 # ---------------------------------------------
 
 tm_segment() {
-  local -r icon=$1
+  local -r text=$1
   local -r color=$2
-  local -r text=$3
 
   local res
 
   # Default color
   [[ -z $color ]] \
     && color="colour237"
-
-  # Icon
-  [[ -n $icon ]] \
-    && res+="#[fg=${color}, noreverse]${icon} "
 
   # Text
   [[ -n $text ]] \
@@ -31,6 +26,19 @@ tm_divider() {
   echo -ne "#{window-status-separator}"
 }
 
+# ---------------------------------------------
+
+# Detect if headphones are connected
+# This is for macOS only
+segment_headphones() {
+  [[ "$(uname -s)" == "Darwin" ]] \
+    || return 1
+
+  if system_profiler SPAudioDataType | grep Headphones &> /dev/null; then
+    tm_segment "0(o.o)0" "cyan"
+    tm_divider
+  fi
+}
 
 # ---------------------------------------------
 
@@ -57,50 +65,9 @@ segment_music() {
     spotify="$(echo "$spotify" | cut -c 1-25)..."
 
   if [[ -n "$spotify" ]]; then
-    tm_segment "$music_icon" "cyan" "$spotify"
+    tm_segment "$music_icon $spotify" "cyan"
     tm_divider
   fi
-}
-
-# ---------------------------------------------
-
-# Adapted from yoavmmn's dotfiles:
-# https://github.com/yoavmmn/dotfiles/blob/master/tmux/tmux_local/right_segments.sh#L23-L60
-
-_load_weather() {
-  curl -s http://wttr.in/Tel%20Aviv\?0TmQ > "$TMP_WEATHER_FILE"
-  date +%s >> "$TMP_WEATHER_FILE"
-}
-
-# Weather
-segment_weather() {
-  local -r TMP_WEATHER_FILE="$HOME/.tmux_weather.tmp"
-  local -r REFRESH_RATE=$(( 20 * 60 ))
-  local -r HOT_POINT=17
-
-  [[ -f "$TMP_WEATHER_FILE" ]] \
-    || _load_weather
-
-  local last_epoch; last_epoch="$(tail -n 1 "$TMP_WEATHER_FILE")"
-  local epoch; epoch="$(date +%s)"
-  local delta; delta=$(( epoch - last_epoch ))
-
-  [[ $delta -gt $REFRESH_RATE ]] \
-    && _load_weather
-
-  local weather; weather="$(grep -o "[0-9]* °C" < "$TMP_WEATHER_FILE")"
-  local temprature; temprature=$(echo "$weather" | grep -o "[0-9]*")
-
-  local weather_icon="❆"
-  local weather_color="blue"
-
-  if [[ "$temprature" -gt "$HOT_POINT" ]]; then
-    weather_icon="☀"
-    weather_color="yellow"
-  fi
-
-  tm_segment "$weather_icon " "$weather_color" "$weather"
-  tm_divider
 }
 
 # ---------------------------------------------
@@ -116,7 +83,7 @@ segment_battery() {
 
   [[ $battery_status == 'discharging;' ]] && battery_color="magenta"
 
-  tm_segment "ϟ" "$battery_color" "${battery_percentage%?}"
+  tm_segment "ϟ ${battery_percentage%?}" "$battery_color"
   tm_divider
 }
 
@@ -124,7 +91,7 @@ segment_battery() {
 
 # Date and time
 segment_date() {
-  tm_segment "" "brightblack" "$(date +'%d %b %Y %H:%M')"
+  tm_segment "$(date +'%d %b %Y %H:%M')" "brightblack"
   tm_divider
 }
 
@@ -138,13 +105,13 @@ segment_host() {
     && user="$(whoami)@"
 
 
-  tm_segment "" "colour243" "$user#h"
+  tm_segment "$user#h" "colour243"
 }
 
 # ---------------------------------------------
 
+segment_headphones
 segment_music
-# segment_weather
 segment_battery
 segment_date
 segment_host
