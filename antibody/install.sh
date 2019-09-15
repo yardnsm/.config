b0948245c02a308a45c04dfd5955f32017687434
Yarden Sod-Moriah <yardnsm@gmail.com>
2019-09-15 20:03:53 +0300

    antibody, fnm: minor fixes

--- antibody/install.sh
+++ antibody/install.sh
@@ -9,7 +9,7 @@ main() {
   print_info "Installing Antibody"
 
   if ! cmd_exists 'antibody'; then
-    execute "curl -s https://raw.githubusercontent.com/getantibody/installer/master/install | bash -s" \
+    execute "curl -s https://raw.githubusercontent.com/getantibody/installer/master/install | bash -s - -b /usr/local/bin" \
       "Installing Antibody"
   else
     print_success 'Antibody is already installed'
