90cf01ae1d120d4ff86b8b158736c8653d7ec6ac
Yarden Sod-Moriah <yardnsm@gmail.com>
2019-12-21 01:09:46 +0200

    git: added gsync alias

--- git/functions.zsh
+++ git/functions.zsh
@@ -5,3 +5,9 @@ function gitzip() {
 function gittgz() {
   git archive -o $(basename `git rev-parse --show-toplevel`).tgz HEAD
 }
+
+function gsync() {
+  git add .
+  git commit -m "Sync ($(date))"
+  git push origin master
+}
