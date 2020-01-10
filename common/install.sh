771bb2f99331e1842aa85e9e9e2dd48e107028cc
Yarden Sod-Moriah <yardnsm@gmail.com>
2020-01-10 15:49:04 +0200

    fontconfig: added a new topic

--- common/install.sh
+++ common/install.sh
@@ -7,37 +7,24 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 declare -a SYMLINKS=(
   "../alacritty/.alacritty.yml"
-
   "../common/.hushlogin"
-
   "../compton, .config/compton"
-
   "../dunst, .config/dunst"
-
+  "../fontconfig, .config/fontconfig"
   "../git/.gitconfig"
   "../git/.gitignore_global"
-
   "../gpg/gpg-agent.conf, .gnupg/gpg-agent.conf"
-
   "../i3, .config/i3"
-
   "../nvim/.vimrc"
   "../nvim/nvim.conf, .config/nvim"
-
   "../rofi, .config/rofi"
-
   "../stylelint/.stylelint"
-
   "../terminfo/tmux-256color.terminfo, .terminfo/74/tmux-256color"
   "../terminfo/xterm-256color-italic.terminfo, .terminfo/78/xterm-256color-italic"
-
   "../ternjs/.tern-config"
-
   "../tmux/.tmux.conf"
-
-  "../xorg/.xinitrc"
   "../xorg/.Xresources"
-
+  "../xorg/.xinitrc"
   "../zsh/.zprofile"
   "../zsh/.zshrc"
 )
