b6ed77796595f4721cc80c93aabfc9c8ca5d93b1
Yarden Sod-Moriah <yardnsm@gmail.com>
2020-04-09 02:21:23 +0300

    nvim: move files to the root topic directory

--- nvim/install.sh
+++ nvim/install.sh
@@ -5,7 +5,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 # --------------------------------------------------------------------------------------------------
 
-declare -r VIM_PLUG_PATH="$DOTFILES/nvim/nvim.conf/autoload/plug.vim"
+declare -r VIM_PLUG_PATH="$DOTFILES/nvim/autoload/plug.vim"
 declare -r VIM_PLUG_FILE="https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
 
 declare -r COC_DIR="$HOME/.config/coc/extensions"
