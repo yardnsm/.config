2d2f84453582c48330804540cca1642deaece686
Yarden Sod-Moriah <yardnsm@gmail.com>
2020-04-08 19:43:32 +0300

    git: combine .zsh files to init.zsh

--- git/init.zsh
+++ git/init.zsh
@@ -1,3 +1,17 @@
+function gitzip() {
+  git archive -o $(basename `git rev-parse --show-toplevel`).zip HEAD
+}
+
+function gittgz() {
+  git archive -o $(basename `git rev-parse --show-toplevel`).tgz HEAD
+}
+
+function gsync() {
+  git add .
+  git commit -m "Sync ($(date))"
+  git push origin master
+}
+
 alias g='git'
 alias gs='git status -sb'
 alias ga='git add'
