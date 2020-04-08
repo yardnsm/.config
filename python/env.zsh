e3d962a58016c421186d06fe196f8825b97023a1
Yarden Sod-Moriah <yardnsm@gmail.com>
2020-04-08 19:45:56 +0300

    python: combine .zsh files to init.zsh

--- python/env.zsh
+++
@@ -1,8 +0,0 @@
-# pyenv root
-export PYENV_ROOT="$HOME/.pyenv"
-
-# pyenv shims
-export PATH=$PYENV_ROOT/bin:$PATH
-
-# pyenv default version
-export PYENV_VERSION="3.6.3"
--- python/init.zsh
+++ python/init.zsh
@@ -1,3 +1,12 @@
+# pyenv root
+export PYENV_ROOT="$HOME/.pyenv"
+
+# pyenv shims
+export PATH=$PYENV_ROOT/bin:$PATH
+
+# pyenv default version
+export PYENV_VERSION="3.6.3"
+
 # Lazy load pyenv
 if command -v "pyenv" &> /dev/null; then
   function pyenv() {
