53e8bbd5de5bbf22647f66dc2d2c498bff88be77
Yarden Sod-Moriah <yardnsm@gmail.com>
2020-04-08 19:46:31 +0300

    ruby: combine .zsh files to init.zsh

--- ruby/env.zsh
+++
@@ -1,5 +0,0 @@
-# rbenv shims
-export PATH=~/.rbenv/shims:$PATH
-
-# rbenv version
-export RBENV_VERSION="2.2.4"
--- ruby/init.zsh
+++ ruby/init.zsh
@@ -1,3 +1,9 @@
+# rbenv shims
+export PATH=~/.rbenv/shims:$PATH
+
+# rbenv version
+export RBENV_VERSION="2.2.4"
+
 # Lazy load rbenv
 if command -v "rbenv" &> /dev/null; then
   function rbenv() {
