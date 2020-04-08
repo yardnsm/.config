e8d2588bf56bd80ed108026fa65e466c2c1bc405
Yarden Sod-Moriah <yardnsm@gmail.com>
2020-04-08 19:45:05 +0300

    fzf: combine .zsh files to init.zsh

--- fzf/env.zsh
+++
@@ -1,5 +0,0 @@
-# PATH
-export PATH=/usr/local/opt/fzf/bin:$PATH
-
-# fzf options
-export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -g ""'
--- fzf/init.zsh
+++ fzf/init.zsh
@@ -1,5 +1,10 @@
 # vim: set foldmethod=marker foldlevel=0:
 
+export PATH=/usr/local/opt/fzf/bin:$PATH
+
+# fzf options
+export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -g ""'
+
 # Autocompletion {{{
 
 [[ $- == *i* ]] \
